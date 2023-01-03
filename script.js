64194683567794c948fd7414140f3e39dcbdb829
Michael Lei <michaelleie0ca@gmail.com>
2023-01-02 20:06:37 -0800

    Finish random gif generator

---
+++ script.js
@@ -0,0 +1,14 @@
+const img = document.querySelector("img");
+const search = document.getElementById("search");
+
+const searchImage = () => getImage(search.value);
+
+const getImage = (searchInput) => {
+	const API_URL = `https://api.giphy.com/v1/gifs/translate?api_key=Eb8S55dwVLwdMI5sv3ObTCsbP9O4Gto0&s=${searchInput}`;
+	console.log(searchInput);
+	fetch(API_URL)
+		.then((res) => res.json())
+		.then((res) => {
+			img.src = res.data.images.original.url;
+		});
+};
