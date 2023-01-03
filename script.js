03d485b4a1df6caaa5739c4b9f87ddf1a80a3141
Michael Lei <michaelleie0ca@gmail.com>
2023-01-02 20:49:38 -0800

    Add error handling

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ const searchImage = () => getImage(search.value);
 const getImage = (searchInput) => {
 	const API_URL = `https://api.giphy.com/v1/gifs/translate?api_key=Eb8S55dwVLwdMI5sv3ObTCsbP9O4Gto0&s=${searchInput}`;
 	console.log(searchInput);
-	fetch(API_URL)
+	fetch(API_URL, { mode: "cors" })
 		.then((res) => res.json())
 		.then((res) => {
-			img.src = res.data.images.original.url;
+			const imgURL = res.data.images.original.url;
+
+			img.src = imgURL;
+			img.style.border = "solid 5px var(--secondary-color)";
+		})
+		.catch(() => {
+			img.src = "./error.jpg";
 			img.style.border = "solid 5px var(--secondary-color)";
 		});
 };
