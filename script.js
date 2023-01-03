6da82c4f869c7307dfd22cb2b2d1a36d64acb93f
Michael Lei <michaelleie0ca@gmail.com>
2023-01-02 21:21:55 -0800

    Convert promise based code into async/await

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ const input = document.getElementById("search");
 
 const generateImg = () => getImg(input.value);
 
-const getImg = (searchInput) => {
-	const API_URL = `https://api.giphy.com/v1/gifs/translate?api_key=Eb8S55dwVLwdMI5sv3ObTCsbP9O4Gto0&s=${searchInput}`;
-	fetch(API_URL, { mode: "cors" })
-		.then((res) => res.json())
-		.then((res) => {
-			const imgURL = res.data.images.original.url;
-			img.src = imgURL;
-			img.style.border = "solid 5px var(--secondary-color)";
-		})
-		.catch(() => {
-			img.src = "./error.jpg";
-			img.style.border = "solid 5px var(--secondary-color)";
-		});
+const getImg = async (searchInput) => {
+	try {
+		const API_URL = `https://api.giphy.com/v1/gifs/translate?api_key=Eb8S55dwVLwdMI5sv3ObTCsbP9O4Gto0&s=${searchInput}`;
+		const res = await fetch(API_URL, { mode: "cors" });
+		const imgData = await res.json();
+		const imgURL = imgData.data.images.original.url;
+		img.src = imgURL;
+		img.style.border = "solid 5px var(--secondary-color)";
+	} catch (error) {
+		img.src = "./error.jpg";
+		img.style.border = "solid 5px var(--secondary-color)";
+	}
 };
 
 input.addEventListener("keyup", (e) => {
