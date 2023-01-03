6090493eff5fa9e46f0063ddf364759ddf185fa5
Michael Lei <michaelleie0ca@gmail.com>
2023-01-02 20:56:40 -0800

    Add functionality for enter key to generate gif

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const img = document.querySelector("img");
-const search = document.getElementById("search");
+const input = document.getElementById("search");
 
-const searchImage = () => getImage(search.value);
+const generateImg = () => getImg(input.value);
 
-const getImage = (searchInput) => {
+const getImg = (searchInput) => {
 	const API_URL = `https://api.giphy.com/v1/gifs/translate?api_key=Eb8S55dwVLwdMI5sv3ObTCsbP9O4Gto0&s=${searchInput}`;
 	console.log(searchInput);
 	fetch(API_URL, { mode: "cors" })
 		.then((res) => res.json())
 		.then((res) => {
 			const imgURL = res.data.images.original.url;
-
 			img.src = imgURL;
 			img.style.border = "solid 5px var(--secondary-color)";
 		})
@@ -19,3 +18,9 @@ const getImage = (searchInput) => {
 			img.style.border = "solid 5px var(--secondary-color)";
 		});
 };
+
+input.addEventListener("keyup", (e) => {
+	if (e.key == "Enter") {
+		generateImg();
+	}
+});
