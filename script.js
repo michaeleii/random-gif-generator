a3e1ab9bfe0f388e4b785022cf0e71eb2822e566
Michael Lei <michaelleie0ca@gmail.com>
2023-01-02 21:07:14 -0800

    Remove console.log

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const generateImg = () => getImg(input.value);
 
 const getImg = (searchInput) => {
 	const API_URL = `https://api.giphy.com/v1/gifs/translate?api_key=Eb8S55dwVLwdMI5sv3ObTCsbP9O4Gto0&s=${searchInput}`;
-	console.log(searchInput);
 	fetch(API_URL, { mode: "cors" })
 		.then((res) => res.json())
 		.then((res) => {
